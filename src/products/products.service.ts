93dc92f0f2f47b0f4849a45093945df2bb20f1c0
soulLeo77 <176849955+soulLeo77@users.noreply.github.com>
2026-04-15 11:22:08 -0500

    Handle exception in findOne

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,13 +1,13 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { PrismaClient } from 'generated/prisma/client';
 import { PrismaService } from 'src/prisma.service';
 import { PaginationDto } from 'src/common';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   create(createProductDto: CreateProductDto) {
     return this.prisma.product.create({
@@ -40,7 +40,10 @@ export class ProductsService {
     });
 
     if ( !product ) {
-      throw new NotFoundException(`Product with id #${id} not found`);
+      throw new RpcException({
+        message: `Product with id #${id} not found`,
+        status: HttpStatus.BAD_REQUEST,
+      });
     }
 
     return product;
